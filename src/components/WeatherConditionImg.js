26f0e208544ea6f3af8a780eecff290382be01cb
Wille Marcel <wille.yyz@gmail.com>
2021-06-13 23:07:16 -0300

    Increase small size of WeatherConditionImg

--- src/components/WeatherConditionImg.js
+++ src/components/WeatherConditionImg.js
@@ -8,7 +8,7 @@ import { ReactComponent as Cloud } from './assets/cloud.svg';
  * Available size options are large and small (default).
 */
 export const WeatherConditionImg = ({ condition, size='small' }) => {
-  const sizeClass = size === 'small' ? 'w-16' : 'w-52';
+  const sizeClass = size === 'small' ? 'w-20' : 'w-52';
 
   if (condition === 'Clear') {
     return <Sun className={`text-yellow ${sizeClass}`} />
