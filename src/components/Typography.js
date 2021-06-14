40249803f56a69b149bb73fafc95a1ef043c8ed0
Wille Marcel <wille.yyz@gmail.com>
2021-06-13 23:07:47 -0300

    Add level 3 option to Title component

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -5,6 +5,7 @@ export const Title = ({ level=1, children }) => {
   const sizeClass = {
     1: 'text-10xl',
     2: 'text-6xl',
+    3: 'text-5xl',
   };
   return <span className={`text-white font-bold ${sizeClass[level]}`}>{ children }</span>;
 }
--- src/components/tests/Typography.test.js
+++ src/components/tests/Typography.test.js
@@ -7,18 +7,26 @@ test('Text component is correctly formated', () => {
   expect(screen.getByText('Hello World').className).toBe('text-indigo-30 text-3xl');
 });
 
-test('Title component with level 1 is corrected formatted', () => {
-  render(<Title level={1}>16º</Title>);
-  const title = screen.getByText('16º');
-  expect(title.className).toContain('text-white');
-  expect(title.className).toContain('font-bold');
-  expect(title.className).toContain('text-10xl');
-});
-
-test('Title component with level 2 is corrected formatted', () => {
-  render(<Title level={2}>Monday</Title>);
-  const title = screen.getByText('Monday');
-  expect(title.className).toContain('text-white');
-  expect(title.className).toContain('font-bold');
-  expect(title.className).toContain('text-6xl');
+describe('Title component', () => {
+  it('with level 1 is corrected formatted', () => {
+    render(<Title level={1}>16º</Title>);
+    const title = screen.getByText('16º');
+    expect(title.className).toContain('text-white');
+    expect(title.className).toContain('font-bold');
+    expect(title.className).toContain('text-10xl');
+  });
+  it('with level 2 is corrected formatted', () => {
+    render(<Title level={2}>Monday</Title>);
+    const title = screen.getByText('Monday');
+    expect(title.className).toContain('text-white');
+    expect(title.className).toContain('font-bold');
+    expect(title.className).toContain('text-6xl');
+  });
+  it('with level 3 is corrected formatted', () => {
+    render(<Title level={3}>Tuesday</Title>);
+    const title = screen.getByText('Tuesday');
+    expect(title.className).toContain('text-white');
+    expect(title.className).toContain('font-bold');
+    expect(title.className).toContain('text-5xl');
+  });
 });
